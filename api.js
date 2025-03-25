05a6fc8791192c9d2d8639f5a62edb70909b1796
Sanjay@419 <136907525+sanjay-419@users.noreply.github.com>
2025-03-25 15:29:38 +0530

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with base URL
 const api = axios.create({
-  baseURL: "http://localhost:5500", // Change to match your backend port
+  baseURL: "https://8cf2-49-207-184-78.ngrok-free.app", // Change to match your backend port
   headers: {
     "Content-Type": "application/json",
   },
